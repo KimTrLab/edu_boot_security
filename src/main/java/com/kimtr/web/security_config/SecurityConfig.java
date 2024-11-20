2891ac002803f97c881357478a4e95b254a3e3bc
kimtr <kk@kk.com>
2024-11-20 17:19:07 +0900

    .

--- src/main/java/com/kimtr/web/security_config/SecurityConfig.java
+++ src/main/java/com/kimtr/web/security_config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.kimtr.web.security_config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
